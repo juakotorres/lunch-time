b499d9297aafbff29025d95aa4d2ffd2a7c88e75
Joaquín Torres <jtorresparis@gmail.com>
2025-05-26 23:31:32 +0900

    chore: Add eslint and prettier to keep code quality

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // After React 17+ there is no need for React import
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+};
