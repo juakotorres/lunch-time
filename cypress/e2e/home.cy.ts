6e5733286d770950846c82c5ea0c10c798b5e409
Joaquín Torres <jtorresparis@gmail.com>
2025-05-28 22:08:33 +0900

    feat: Structure project with left panel and a map.

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,15 @@
 describe('Home Page', () => {
-  it('shows the main page icon', () => {
+  it('correctly loads left panel and map', () => {
     cy.visit('/');
-    cy.dataCy('logo-icon').should('be.visible');
+    cy.dataCy('home-page')
+      .should('be.visible')
+      .within(() => {
+        cy.dataCy('left-panel')
+          .should('be.visible')
+          .within(() => {
+            cy.dataCy('logo-icon').should('be.visible');
+          });
+        cy.dataCy('map-container').should('be.visible');
+      });
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import logo from './logo.png';
-import './App.css';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
-  return (
-    <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" data-cy="logo-icon" />
-        <p>Lunch Time!</p>
-      </header>
-      <footer>
-        <a
-          href="https://www.flaticon.com/free-icons/restaurant"
-          title="restaurant icons"
-          className="app-footer-link"
-        >
-          Restaurant icons created by Freepik - Flaticon
-        </a>
-      </footer>
-    </div>
-  );
+  return <HomePage />;
 }
 
 export default App;
